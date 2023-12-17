2b384e3ccf39d61f2e721c4ce4967604a435fb83
nhkim <nhkim1529@gmail.com>
2023-12-17 22:56:32 +0900

    Feat: 홈화면 로직 코드 작성

---
+++ selfintroduction/app/src/main/java/com/nhkim/selfintroduction/HomeActivity.kt
@@ -0,0 +1,32 @@
+package com.nhkim.selfintroduction
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import org.w3c.dom.Text
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+        val idText = findViewById<TextView>(R.id.homeId)
+        val nameText = findViewById<TextView>(R.id.homeName)
+        val finishBtn = findViewById<Button>(R.id.finishBtn)
+
+        val userId = intent.getStringExtra("userId")
+        val userName = intent.getStringExtra("userName")
+        idText.text = "아이디 : $userId"
+        if(userName != null){
+            nameText.text = "이름 : $userName"
+        }
+
+        finishBtn.setOnClickListener {
+            finish()
+        }
+
+    }
+}
