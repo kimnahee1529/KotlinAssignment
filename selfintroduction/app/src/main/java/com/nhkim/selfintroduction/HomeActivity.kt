f0289c3cd5ec10c8f063c662f238006b0c5a1734
nhkim <nhkim1529@gmail.com>
2023-12-19 16:40:20 +0900

    Feat: 로그인할 때마다 랜덤으로 프로필 이미지 생성

--- selfintroduction/app/src/main/java/com/nhkim/selfintroduction/HomeActivity.kt
+++ selfintroduction/app/src/main/java/com/nhkim/selfintroduction/HomeActivity.kt
@@ -4,25 +4,35 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import org.w3c.dom.Text
+import java.util.Random
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
+        val imageList = listOf(R.drawable.codinge1, R.drawable.codinge2,R.drawable.codinge3, R.drawable.codinge4, R.drawable.codinge5)
+
         val idText = findViewById<TextView>(R.id.homeId)
         val nameText = findViewById<TextView>(R.id.homeName)
         val finishBtn = findViewById<Button>(R.id.finishBtn)
+        val profileImage = findViewById<ImageView>(R.id.homeImageView)
+        val random: Random = Random()
+        val num = random.nextInt(5)
 
         val userId = intent.getStringExtra("userId")
         val userName = intent.getStringExtra("userName")
+
         idText.text = "아이디 : $userId"
         if(userName != null){
             nameText.text = "이름 : $userName"
         }
+
+        profileImage.setImageResource(imageList[num])
 
         finishBtn.setOnClickListener {
             finish()
