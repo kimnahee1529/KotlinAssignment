51234613dca5a99f49944b7acc0a5e4eadaa6091
nhkim <nhkim1529@gmail.com>
2024-01-28 23:59:27 +0900

    Feat: Data class 추가

---
+++ ImageCollector/app/src/main/java/com/nhkim/imagecollector/data/Document.kt
@@ -0,0 +1,14 @@
+package com.nhkim.imagecollector.data
+
+import java.util.Date
+
+data class Document(
+    val collection: String,
+    val thumbnail_url: String,
+    val image_url: String,
+    val width: Int,
+    val height: Int,
+    val display_sitename: String,
+    val doc_url: String,
+    val datetime: Date
+)
