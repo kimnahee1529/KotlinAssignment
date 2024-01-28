86376dfb33adc077b0db050dd3f6684de5dcbbde
nhkim <nhkim1529@gmail.com>
2024-01-29 00:07:02 +0900

    Feat: 검색 프래그먼트 수정

--- ImageCollector/app/src/main/java/com/nhkim/imagecollector/ImageSearchFragment.kt
+++ ImageCollector/app/src/main/java/com/nhkim/imagecollector/ImageSearchFragment.kt
@@ -1,59 +1,137 @@
 package com.nhkim.imagecollector
 
+import android.app.Activity
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.GridLayoutManager
+import com.nhkim.imagecollector.ImageSearchFragment.UtilityKeyboard.hideKeyboard
+import com.nhkim.imagecollector.data.Document
+import com.nhkim.imagecollector.databinding.FragmentImageSearchBinding
+import com.nhkim.imagecollector.retrofit.NetWorkClient
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.lang.Exception
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ImageSearchFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ImageSearchFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    var items = listOf<Document>()
+
+    private var _binding: FragmentImageSearchBinding? = null
+    private val binding get() = _binding!!
+    private val imageAdapter by lazy { ImageAdapter() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_image_search, container, false)
+        _binding = FragmentImageSearchBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ImageSearchFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ImageSearchFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        loadData()
+
+        val mainActivity = activity as? MainActivity
+        val sharedList = mainActivity?.sharedList
+
+        binding.btnSearch.setOnClickListener {
+            val searchText = binding.etSearch.text.toString()
+            Log.d("searchText", searchText)
+            communicateNetWork(searchText)
+            saveData()
+            this.hideKeyboard()
+        }
+
+        // Adapter 설정
+        imageAdapter.itemClick = object : ImageAdapter.ItemClick {
+            override fun onClick(view: View, position: Int) {
+                Log.d("item onClick1", position.toString()+"번째 아이템 클릭")
+                val clickedItem = imageAdapter.getDocumentAtPosition(position)
+                if (clickedItem != null) {
+                    Log.d("item onClick2", clickedItem.thumbnail_url)
+                    sharedList?.add(clickedItem.thumbnail_url)
+                    Log.d("item onClick3", sharedList.toString())
+                }
+//                Log.d("item onClick2", items[position].thumbnail_url)
+//                sharedList?.add(items[position].thumbnail_url)
+//                sharedList.let{
+//                    Log.d("item onClick", items[position].thumbnail_url)
+//                }
+            }
+//            override fun onLongClick(view: View, position: Int): Boolean {
+//                // 아이템 롱 클릭 시 동작
+//
+//                return true
+//            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+    private fun communicateNetWork(search: String) = lifecycleScope.launch {
+        try{
+            val authKey = "KakaoAK ${NetWorkClient.apiKey}"
+            val responseData = NetWorkClient.imageNetWork.getThumbnailImage(authKey, search, size = 80)
+            Log.d("responseData", responseData.documents.toString())
+
+//            items = responseData.documents
+
+            withContext(Dispatchers.Main){
+                imageAdapter.submitList(responseData.documents)
+                if(binding.rvImage.layoutManager == null){
+                    binding.rvImage.layoutManager = GridLayoutManager(context, 2)
+                    binding.rvImage.adapter = imageAdapter
                 }
             }
+
+        } catch (e: Exception){
+            Log.e("NetworkError", "Failed to fetch data", e)
+        }
+    }
+    object UtilityKeyboard {
+        fun Fragment.hideKeyboard() {
+            view?.let { activity?.hideKeyboard(it) }
+        }
+
+        fun Activity.hideKeyboard() {
+            hideKeyboard(currentFocus ?: View(this))
+        }
+
+        fun Context.hideKeyboard(view: View) {
+            val inputMethodManager =
+                getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+        }
+    }
+
+    private fun saveData() {
+        val pref = activity?.getSharedPreferences("pref", Context.MODE_PRIVATE)
+        val edit = pref?.edit()
+        edit?.putString("name", binding.etSearch.text.toString())
+        edit?.apply()
+    }
+
+    private fun loadData() {
+        val pref = activity?.getSharedPreferences("pref", Context.MODE_PRIVATE)
+        val name = pref?.getString("name", "")
+        binding.etSearch.setText(name)
     }
 }
