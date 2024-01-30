20ceb275ac3578360f02c48aafa27a5398572497
nhkim <nhkim1529@gmail.com>
2024-01-30 23:32:37 +0900

    Feat: 좋아요 상태 저장 변수 추가

--- ImageCollector/app/src/main/java/com/nhkim/imagecollector/data/Document.kt
+++ ImageCollector/app/src/main/java/com/nhkim/imagecollector/data/Document.kt
@@ -3,12 +3,13 @@ package com.nhkim.imagecollector.data
 import java.util.Date
 
 data class Document(
-    val collection: String,
+    val collection: String = "",
     val thumbnail_url: String,
-    val image_url: String,
-    val width: Int,
-    val height: Int,
+    val image_url: String = "",
+    val width: Int = 0,
+    val height: Int = 0,
     val display_sitename: String,
-    val doc_url: String,
-    val datetime: Date
+    val doc_url: String = "",
+    val datetime: Date,
+    var isHearted: Boolean = false
 )
