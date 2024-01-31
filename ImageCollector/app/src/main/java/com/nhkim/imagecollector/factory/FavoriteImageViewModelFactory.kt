e1490e488d1507d5445af9e91d8de8974fee2f92
nhkim <nhkim1529@gmail.com>
2024-02-01 02:27:15 +0900

    Refactor: FavoriteImageViewModelFactory 리팩토링

--- ImageCollector/app/src/main/java/com/nhkim/imagecollector/factory/FavoriteImageViewModelFactory.kt
+++ ImageCollector/app/src/main/java/com/nhkim/imagecollector/factory/FavoriteImageViewModelFactory.kt
@@ -3,19 +3,17 @@ package com.nhkim.imagecollector.factory
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import com.nhkim.imagecollector.repository.ImageRepository
 import com.nhkim.imagecollector.repository.PreferencesRepository
 import com.nhkim.imagecollector.ui.favorites.FavoritesViewModel
 
 class FavoriteImageViewModelFactory(
-    private val repository: ImageRepository,
     private val preferencesRepository: PreferencesRepository
 ) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         Log.d("Factory", "searchImages")
         if (modelClass.isAssignableFrom(FavoritesViewModel::class.java)) {
             @Suppress("UNCHECKED_CAST")
-            return FavoritesViewModel(repository, preferencesRepository) as T
+            return FavoritesViewModel(preferencesRepository) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")
     }
