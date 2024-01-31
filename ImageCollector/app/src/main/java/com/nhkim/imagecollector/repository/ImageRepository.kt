564cc76fc609d2916016208d41b4e44679acd4b9
nhkim <nhkim1529@gmail.com>
2024-02-01 02:48:51 +0900

    Comment: 주석 삭제

--- ImageCollector/app/src/main/java/com/nhkim/imagecollector/repository/ImageRepository.kt
+++ ImageCollector/app/src/main/java/com/nhkim/imagecollector/repository/ImageRepository.kt
@@ -5,11 +5,9 @@ import com.nhkim.imagecollector.data.ImageResponse
 import com.nhkim.imagecollector.network.NetWorkClient
 
 class ImageRepository {
-    suspend fun searchImage(search: String): ImageResponse{
+    suspend fun searchImage(search: String): ImageResponse {
         Log.d("ImageRepository", "searchImage")
         val authKey = "KakaoAK ${NetWorkClient.apiKey}"
-        val responseData = NetWorkClient.imageNetWork.getThumbnailImage(authKey, search, size = 80)
-//        Log.d("responseData", responseData.documents.toString())
-        return responseData
+        return NetWorkClient.imageNetWork.getThumbnailImage(authKey, search, size = 80)
     }
 }
