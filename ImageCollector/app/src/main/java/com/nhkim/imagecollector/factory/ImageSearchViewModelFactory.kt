b5e6270b439c6f798fcdad2577acc38c8c097574
nhkim <nhkim1529@gmail.com>
2024-02-01 02:29:44 +0900

    Refactor: ImageSearchViewModelFactory 수정

--- ImageCollector/app/src/main/java/com/nhkim/imagecollector/factory/ImageSearchViewModelFactory.kt
+++ ImageCollector/app/src/main/java/com/nhkim/imagecollector/factory/ImageSearchViewModelFactory.kt
@@ -4,14 +4,18 @@ import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.nhkim.imagecollector.repository.ImageRepository
+import com.nhkim.imagecollector.repository.PreferencesRepository
 import com.nhkim.imagecollector.ui.search.ImageSearchViewModel
 
-class ImageSearchViewModelFactory(private val repository: ImageRepository) : ViewModelProvider.Factory {
+class ImageSearchViewModelFactory(
+    private val imageRepository: ImageRepository,
+    private val preferencesRepository: PreferencesRepository
+) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         Log.d("Factory", "searchImages")
         if (modelClass.isAssignableFrom(ImageSearchViewModel::class.java)) {
             @Suppress("UNCHECKED_CAST")
-            return ImageSearchViewModel(repository) as T
+            return ImageSearchViewModel(imageRepository, preferencesRepository) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")
     }
