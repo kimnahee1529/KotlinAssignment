f8f90f478fffad92740e856a8f12886808d7b0b9
nhkim <nhkim1529@gmail.com>
2024-02-01 11:47:12 +0900

    Feat: ViewPager로 수정

--- ImageCollector/app/src/main/java/com/nhkim/imagecollector/MainActivity.kt
+++ ImageCollector/app/src/main/java/com/nhkim/imagecollector/MainActivity.kt
@@ -2,44 +2,27 @@ package com.nhkim.imagecollector
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.nhkim.imagecollector.data.Document
+import com.google.android.material.tabs.TabLayoutMediator
 import com.nhkim.imagecollector.databinding.ActivityMainBinding
-import com.nhkim.imagecollector.ui.favorites.FavoritesFragment
-import com.nhkim.imagecollector.ui.search.ImageSearchFragment
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    var sharedList: MutableList<Document> = mutableListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val imageSearchFragment = ImageSearchFragment()
+        binding.pager.adapter = ViewPagerAdapter(this)
+        with(binding) {
+            TabLayoutMediator(tabLayout, pager) { tab, position ->
+                when (position) {
+                    0 -> tab.text = "이미지 검색"
+                    1 -> tab.text = "내 보관함"
+                }
+            }.attach()
 
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction().apply {
-                add(R.id.main_fragment, imageSearchFragment)
-                commit()
-            }
-        }
-
-        binding.btnFavorites.setOnClickListener {
-            val favoritesFragment = FavoritesFragment()
-            val transaction = supportFragmentManager.beginTransaction()
-            transaction.replace(R.id.main_fragment, favoritesFragment)
-            transaction.addToBackStack(null)
-            transaction.commit()
-        }
-
-        binding.btnImageSearch.setOnClickListener {
-            val imageSearchFragment = ImageSearchFragment()
-            val transaction = supportFragmentManager.beginTransaction()
-            transaction.replace(R.id.main_fragment, imageSearchFragment)
-            transaction.addToBackStack(null)
-            transaction.commit()
         }
     }
 }
