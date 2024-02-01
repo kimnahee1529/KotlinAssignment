5da7d1f118e0bcd68e8a4fe5ab239d7b2ecfd77f
nhkim <nhkim1529@gmail.com>
2024-02-01 11:46:56 +0900

    Feat: SharedPreference에 값 저장하는 PreferencesRepository 추가

--- ImageCollector/app/src/main/java/com/nhkim/imagecollector/repository/PreferencesRepository.kt
+++ ImageCollector/app/src/main/java/com/nhkim/imagecollector/repository/PreferencesRepository.kt
@@ -7,13 +7,6 @@ import com.nhkim.imagecollector.data.Document
 
 class PreferencesRepository(private val sharedPreferences: SharedPreferences) {
     private val favoriteKey = "saveFavoritesData"
-    fun saveFavoritesDataList(userDataList: List<Document>) {
-        val editor = sharedPreferences.edit()
-        val gson = Gson()
-        val json = gson.toJson(userDataList)
-        editor.putString(favoriteKey, json)
-        editor.apply()
-    }
 
     fun loadFavoritesDataList(): List<Document> {
         val json = sharedPreferences.getString(favoriteKey, null)
@@ -23,6 +16,45 @@ class PreferencesRepository(private val sharedPreferences: SharedPreferences) {
         } else {
             emptyList()
         }
+    }
+
+    fun toggleFavorite(document: Document) {
+        val currentFavorites = getFavoritesDataList() // 현재 저장된 즐겨찾기 리스트를 가져옴
+        val editor = sharedPreferences.edit()
+        val gson = Gson()
+
+        var foundDocument: Document? = null
+
+        for (favDoc in currentFavorites) {
+            if (favDoc.thumbnail_url == document.thumbnail_url) {
+                foundDocument = favDoc
+                break
+            }
+        }
+
+        if (foundDocument != null) {
+            currentFavorites.remove(foundDocument)
+        } else {
+            currentFavorites.add(document)
+        }
+
+        val json = gson.toJson(currentFavorites)
+        editor.putString(favoriteKey, json)
+        editor.apply()
+    }
+
+    fun getFavoritesDataList(): MutableList<Document> {
+        val json = sharedPreferences.getString(favoriteKey, null)
+        val type = object : TypeToken<List<Document>>() {}.type
+        return Gson().fromJson(json, type) ?: mutableListOf()
+    }
+
+    fun deleteFavorite(document: Document) {
+        val currentFavorites = getFavoritesDataList()
+        val gson = Gson()
+        currentFavorites.remove(document)
+        val json = gson.toJson(currentFavorites)
+        sharedPreferences.edit().putString(favoriteKey, json).apply()
     }
 
 }
