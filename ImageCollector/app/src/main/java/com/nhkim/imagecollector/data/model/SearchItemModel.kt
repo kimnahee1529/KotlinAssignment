0eeee87e30da161cd5a6feb538d2e462d790a0c4
nhkim <nhkim1529@gmail.com>
2024-02-02 20:02:43 +0900

    Feat: dateTime의 타입을 Date로 변경

--- ImageCollector/app/src/main/java/com/nhkim/imagecollector/data/model/SearchItemModel.kt
+++ ImageCollector/app/src/main/java/com/nhkim/imagecollector/data/model/SearchItemModel.kt
@@ -1,7 +1,13 @@
 package com.nhkim.imagecollector.data.model
 
-class SearchItemModel(var type: Int, var title: String, var dateTime: String, var url: String) {
+import java.util.Date
 
+class SearchItemModel(
+    var type: String,
+    var title: String,
+    var dateTime: Date,
+    var url: String
+) {
     // 항목이 '좋아요' 상태인지 나타내는 변수. 기본값은 false.
     var isLike = false
 }
