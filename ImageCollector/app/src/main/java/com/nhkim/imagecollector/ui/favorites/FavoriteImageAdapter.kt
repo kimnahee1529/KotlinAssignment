af4ea0a73eefc0c1b3ecde8e21add0ae4ae07b53
nhkim <nhkim1529@gmail.com>
2024-02-02 19:52:01 +0900

    Design: Image/Video 태그 색 다르게 하기

--- ImageCollector/app/src/main/java/com/nhkim/imagecollector/ui/favorites/FavoriteImageAdapter.kt
+++ ImageCollector/app/src/main/java/com/nhkim/imagecollector/ui/favorites/FavoriteImageAdapter.kt
@@ -4,6 +4,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
@@ -11,10 +12,10 @@ import androidx.recyclerview.widget.RecyclerView
 import com.nhkim.imagecollector.utils.FormatManager
 import com.nhkim.imagecollector.utils.FormatManager.loadImage
 import com.nhkim.imagecollector.R
-import com.nhkim.imagecollector.data.image.Document
+import com.nhkim.imagecollector.data.model.SearchItemModel
 import com.nhkim.imagecollector.databinding.RecyclerViewItemBinding
 
-class FavoriteImageAdapter() : ListAdapter<Document, FavoriteImageAdapter.Holder>(
+class FavoriteImageAdapter() : ListAdapter<SearchItemModel, FavoriteImageAdapter.Holder>(
     DocumentDiffCallback()
 ) {
 
@@ -24,14 +25,14 @@ class FavoriteImageAdapter() : ListAdapter<Document, FavoriteImageAdapter.Holder
 
     var favoriteItemClick: FavoriteItemClick? = null
 
-    class DocumentDiffCallback : DiffUtil.ItemCallback<Document>() {
-        override fun areItemsTheSame(oldItem: Document, newItem: Document): Boolean {
+    class DocumentDiffCallback : DiffUtil.ItemCallback<SearchItemModel>() {
+        override fun areItemsTheSame(oldItem: SearchItemModel, newItem: SearchItemModel): Boolean {
             // 객체의 고유 식별자를 비교
-            return oldItem.thumbnail_url == newItem.thumbnail_url
+            return oldItem.url == newItem.url
         }
 
-        override fun areContentsTheSame(oldItem: Document, newItem: Document): Boolean {
-            return oldItem == newItem
+        override fun areContentsTheSame(oldItem: SearchItemModel, newItem: SearchItemModel): Boolean {
+            return oldItem.title == newItem.title
         }
     }
 
@@ -52,19 +53,28 @@ class FavoriteImageAdapter() : ListAdapter<Document, FavoriteImageAdapter.Holder
     inner class Holder(private val binding: RecyclerViewItemBinding) :
         RecyclerView.ViewHolder(binding.root) {
 
-        fun bind(document: Document) {
-            binding.ivThumbnail.loadImage(document.thumbnail_url)
+        fun bind(document: SearchItemModel) {
+            binding.tvType.text = document.type
+            when(document.type){
+                "Video"->{
+                    binding.tvType.background.setTint(ContextCompat.getColor(binding.root.context, R.color.blue))
+                }
+                "Image"->{
+                    binding.tvType.background.setTint(ContextCompat.getColor(binding.root.context, R.color.pink))
+                }
+            }
+            binding.ivThumbnail.loadImage(document.url)
 
-            Log.d("Adapter썸네일", document.thumbnail_url)
+            Log.d("Adapter썸네일", document.url)
             binding.ivThumbnail.clipToOutline = true
 
-            binding.tvSiteName.text = document.display_sitename
-            FormatManager.formatDateToString(document.datetime)
-            binding.ivHeart.isVisible = document.isHearted
+            binding.tvSiteName.text = document.title
+            FormatManager.formatDateToString(document.dateTime)
+            binding.ivHeart.isVisible = document.isLike
 
 //          // 하트 이미지 클릭 리스너 설정
             binding.root.setOnClickListener {
-                document.isHearted = !document.isHearted
+                document.isLike = !document.isLike
                 binding.ivHeart.setImageResource(R.drawable.full_heart)
                 favoriteItemClick?.onDeleteClick(it, position)
             }
