48305c04dd34a5a4a6566751dc9b9a01ec9fc7d6
nhkim <nhkim1529@gmail.com>
2024-02-03 16:45:31 +0900

    Feat: searchData를 searchImages와 searchVideos로 분리

--- ImageCollector/app/src/main/java/com/nhkim/imagecollector/ui/search/ImageSearchViewModel.kt
+++ ImageCollector/app/src/main/java/com/nhkim/imagecollector/ui/search/ImageSearchViewModel.kt
@@ -17,29 +17,61 @@ class ImageSearchViewModel(
     private val imageRepository: ImageRepository,
     private val preferencesRepository: PreferencesRepository
 ) : ViewModel() {
-
     //이미지 받아오기
     private val _imagesData = MutableLiveData<List<SearchItemModel>>()
     val imagesData: LiveData<List<SearchItemModel>> = _imagesData
+    private var isImageSearchFinished = false
+    private var isVideoSearchFinished = false
+    private val combinedList: ArrayList<SearchItemModel> = ArrayList()
 
     fun searchData(query: String){
+        searchImages(query)
+        searchVideos(query)
+    }
+    private fun searchImages(query: String) {
+        Log.d("viewModel 시작", "searchImages")
         viewModelScope.launch {
-            val imageSearchDeferred = async { imageRepository.searchImage(query) }
-            val videoSearchDeferred  = async { imageRepository.searchVideo(query) }
-
-            kotlin.runCatching {
-                val imageResponse = imageSearchDeferred.await()
-                val videoResponse = videoSearchDeferred.await()
-
-                val imageModels = imageResponse.documents.map { convertToSearchItemModel(it) }
-                val videoModels = videoResponse.documents.map { convertToSearchItemModel(it) }
-
-                val combinedList = (imageModels + videoModels).sortedByDescending { it.dateTime }
-
-                _imagesData.postValue(combinedList)
+            runCatching {
+                val response = imageRepository.searchImage(query)
+                Log.d("viewModel 반환", "searchImages, $response")
+                val searchItemModels = response.documents.map { document ->
+                    convertToSearchItemModel(document)
+                }
+                Log.d("convert 후 viewModel 반환", "searchImages, $searchItemModels")
+                combinedList.addAll(searchItemModels)
+                isImageSearchFinished = true
+                postCombinedDataIfReady()
             }.onFailure {
-                Log.e("TAG", "searchData() failed! : ${it.message}")
+                Log.e("TAG", "fetchTrendingVideos() failed! : ${it.message}")
             }
+        }
+    }
+
+    private fun searchVideos(query: String) {
+        Log.d("viewModel 시작", "searchVideos")
+        viewModelScope.launch {
+            runCatching {
+                val response = imageRepository.searchVideo(query)
+                Log.d("viewModel 반환", "searchVideos, $response")
+                val searchItemModels = response.documents.map { document ->
+                    convertToSearchItemModel(document)
+                }
+                combinedList.addAll(searchItemModels)
+                isVideoSearchFinished = true
+                postCombinedDataIfReady()
+            }.onFailure {
+                Log.e("TAG", "searchVideos() failed! : ${it.message}")
+            }
+        }
+    }
+
+    private fun postCombinedDataIfReady(){
+        if (isImageSearchFinished && isVideoSearchFinished) {
+            _imagesData.postValue(ArrayList(combinedList).sortedByDescending { it.dateTime })
+            // 검색 상태 초기화
+            isImageSearchFinished = false
+            isVideoSearchFinished = false
+            combinedList.clear()
         }
     }
 
@@ -48,7 +80,6 @@ class ImageSearchViewModel(
     }
 
     private fun convertToSearchItemModel(document: ImageDocument): SearchItemModel {
-
         return SearchItemModel(
             type = "Image",
             title = document.display_sitename,
@@ -60,7 +91,6 @@ class ImageSearchViewModel(
     }
 
     private fun convertToSearchItemModel(document: VideoDocument): SearchItemModel {
-
         return SearchItemModel(
             type = "Video",
             title = document.title,
@@ -71,5 +101,3 @@ class ImageSearchViewModel(
         }
     }
 }
-
-
